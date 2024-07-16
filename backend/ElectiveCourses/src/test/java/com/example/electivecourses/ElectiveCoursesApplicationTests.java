f7e30e1e3ea4188f40515a24ae22220a47a5aac3
Robert Alex Barbu <robertalexbarbu@gmail.com>
2024-07-16 17:12:54 +0300

    Project init

---
+++ backend/ElectiveCourses/src/test/java/com/example/electivecourses/ElectiveCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.electivecourses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectiveCoursesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
