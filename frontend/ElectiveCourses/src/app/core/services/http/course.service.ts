4ea63a98b7cb35da8fdf10ea7e70ecfa2e01671f
Robert Alex Barbu <robertalexbarbu@gmail.com>
2024-07-29 16:27:58 +0300

    Frontend: courses create page

--- frontend/ElectiveCourses/src/app/core/services/http/course.service.ts
+++ frontend/ElectiveCourses/src/app/core/services/http/course.service.ts
@@ -1,6 +1,7 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {CourseDto} from "../../dto/course/CourseDto";
+import {CreateCourseDto} from "../../dto/course/CreateCourseDto";
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,8 @@ export class CourseService {
     private readonly path = "http://localhost:8080/courses"
     getAllCourses() {
       return this.http.get<CourseDto[]>(this.path);
+    }
+    createCourse(dto: CreateCourseDto) {
+      return this.http.post(this.path, dto);
     }
 }
--- frontend/ElectiveCourses/src/app/features/course-feature/pages/add-course-page/add-course-page.component.ts
+++ frontend/ElectiveCourses/src/app/features/course-feature/pages/add-course-page/add-course-page.component.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {CourseService} from "../../../../core/services/http/course.service";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatOption, MatSelect} from "@angular/material/select";
+import {MatButton} from "@angular/material/button";
+import {MatListModule} from "@angular/material/list";
+import {Router, RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-add-course-page',
   standalone: true,
-  imports: [],
+    imports: [
+        ReactiveFormsModule,
+        MatFormField,
+        MatInput,
+        MatSelect,
+        MatOption,
+        MatButton,
+        MatLabel,
+        RouterLink
+    ],
   templateUrl: './add-course-page.component.html',
   styleUrl: './add-course-page.component.css'
 })
 export class AddCoursePageComponent {
 
+    private readonly courseService = inject(CourseService);
+    private readonly router = inject(Router);
+    
+    form = new FormGroup({
+        name: new FormControl("", {
+            validators: [Validators.required],
+            nonNullable: true
+        }),
+        maxStudentsAllowed: new FormControl(0, {
+            nonNullable: true
+        }),
+        yearOfStudy: new FormControl(0, {
+            nonNullable: true
+        }),
+        dayOfWeek: new FormControl("", {
+            nonNullable: true,
+            validators: [Validators.required],
+        }),
+        startHour: new FormControl(0, {
+            nonNullable: true
+        }),
+        endHour: new FormControl(0, {
+            nonNullable: true
+        }),
+        
+    })
+    
+    daysOfWeek = [""]
+    submit() {
+        this.courseService.createCourse(this.form.getRawValue()).subscribe({
+            next: value => {
+                this.router.navigateByUrl("/admin/courses");
+            }
+        })
+    }
 }
