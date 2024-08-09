e50dd9fc89890403e9b546bed754407774906792
Patrick Matasaru <patrick.matasaru@gmail.com>
2024-08-09 12:23:39 +0300

    Display course name instead of id in teachers

--- frontend/ElectiveCourses/src/app/features/teacher-feature/pages/teacher-page/teacher-page.component.ts
+++ frontend/ElectiveCourses/src/app/features/teacher-feature/pages/teacher-page/teacher-page.component.ts
@@ -5,6 +5,8 @@ import {MatButton} from "@angular/material/button";
 import {RouterLink} from "@angular/router";
 import {MatTableModule} from '@angular/material/table';
 import {JsonPipe, NgIf} from "@angular/common";
+import {CourseService} from "../../../../core/services/http/course.service";
+import {CourseDto} from "../../../../core/dto/course/CourseDto";
 
 @Component({
   selector: 'app-teacher-page',
@@ -21,22 +23,49 @@ import {JsonPipe, NgIf} from "@angular/common";
 })
 export class TeacherPageComponent implements OnInit {
   teachers: TeacherDto[] = [];
-  displayedColumns: string[] = ['id', 'name', 'courseId'];
+  courses: CourseDto[] = [];
+
+  displayedColumns: string[] = ['id', 'name', 'courseName'];
+  loadingTeachers = true;
+  loadingCourses = true;
   loading = true;
   error: string | null = null;
   private readonly teacherService = inject(TeacherService);
+  private readonly courseService: CourseService = inject(CourseService);
+
 
   ngOnInit() {
+    this.courseService.getAllCourses().subscribe({
+      next: (value) => {
+        this.courses = value;
+        this.loadingCourses = false;
+        this.loading = this.loadingTeachers;
+      },
+      error: (err) => {
+        console.error('Error fetching courses', err);
+        this.error = 'Error fetching courses data';
+        this.loadingCourses = false;
+        this.loading = this.loadingTeachers;
+      }
+    });
+
     this.teacherService.getTeachers().subscribe({
       next: (value) => {
         this.teachers = value;
-        this.loading = false;
+        this.loadingTeachers = false;
+        this.loading = this.loadingCourses;
       },
       error: (err) => {
         console.error('Error fetching teachers', err);
         this.error = 'Error fetching teacher data';
-        this.loading = false;
+        this.loadingTeachers = false;
+        this.loading = this.loadingCourses;
       }
     });
+  }
+
+  getCourseName(courseId: number): string {
+    const course = this.courses.find(course => course.id === courseId);
+    return course ? course.name : 'Unknown';
   }
 }
